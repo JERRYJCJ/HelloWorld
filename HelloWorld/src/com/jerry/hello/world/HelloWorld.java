0c790244f58153eca7528623813daf1034388ddf
JERRY JIANG <jerryjcj@hotmail.com>
2019-01-07 17:37:06 +0800

    upload to jenkins

--- HelloWorld/src/com/jerry/hello/world/HelloWorld.java
+++ HelloWorld/src/com/jerry/hello/world/HelloWorld.java
@@ -6,17 +6,13 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-<<<<<<< HEAD
 		Date date=new Date();
 		
 		System.out.println ("this is hello world sample - jerry - changed 002");
 		
-		System.out.println("upload to git server - 002");
+		System.out.println("upload to git server - 003");
 		
 		System.out.println ("java program is running at minute " + date.getMinutes());
-=======
-		System.out.println ("this is hello world sample - jerry - changed 002");
->>>>>>> branch 'master' of https://github.com/JERRYJCJ/HelloWorld
 
 	}
 
