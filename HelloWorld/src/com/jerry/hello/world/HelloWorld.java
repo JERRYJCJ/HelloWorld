a93dfaaf0c3005e02f2e9fc5cb051b9ff0c6d458
JERRY JIANG <jerryjcj@hotmail.com>
2019-01-07 17:29:11 +0800

    change to test jenkins

--- HelloWorld/src/com/jerry/hello/world/HelloWorld.java
+++ HelloWorld/src/com/jerry/hello/world/HelloWorld.java
@@ -1,10 +1,18 @@
 package com.jerry.hello.world;
 
+import java.util.Date;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println ("this is hello world sample - jerry");
+		Date date=new Date();
+		
+		System.out.println ("this is hello world sample - jerry - changed 002");
+		
+		System.out.println("upload to git server - 002");
+		
+		System.out.println ("java program is running at minute " + date.getMinutes());
 
 	}
 
