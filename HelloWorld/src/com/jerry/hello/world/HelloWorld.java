386bfd13ee3a7addd4357806ba72fb5dd345d439
JERRY JIANG <jerryjcj@hotmail.com>
2019-01-07 17:29:11 +0800

    change to test jenkins

--- HelloWorld/src/com/jerry/hello/world/HelloWorld.java
+++ HelloWorld/src/com/jerry/hello/world/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println ("this is hello world sample - jerry");
+		System.out.println ("this is hello world sample - jerry - changed 002");
 
 	}
 
