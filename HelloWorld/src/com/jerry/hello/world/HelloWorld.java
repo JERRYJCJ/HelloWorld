d42ee58747cd932ba2b54b4e932a20dfdd231c05
JERRY JIANG <jerryjcj@hotmail.com>
2019-01-08 11:18:23 +0800

    display time

--- HelloWorld/src/com/jerry/hello/world/HelloWorld.java
+++ HelloWorld/src/com/jerry/hello/world/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.jerry.hello.world;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class HelloWorld {
@@ -7,14 +9,21 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		Date date=new Date();
+		Calendar cl=Calendar.getInstance();
 		
-		System.out.println ("this is hello world sample - jerry - changed 002");
+		
+		System.out.println ("this is hello world sample - jerry ");
 		 
-		System.out.println("upload to git server - 006");
+		System.out.println("upload to git server - 08Jan 001");
   
 		
-		System.out.println ("java program is running at minute " + date.getMinutes());
+		System.out.println ("java program is running at minute " + cl.getTime() + " ------" + date.getMinutes());
 
+		
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+		System.out.println(df.format(new Date()));// new Date()为获取当前系统时间
+		
+		
 	}
 
 }
